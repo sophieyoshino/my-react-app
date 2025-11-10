d9c40731976eac232f0c2a70de2bdfa01df77e9f
Sophie Yoshino <s.s.yoshino@wustl.edu>
2025-11-10 13:17:59 -0600

    square spelled correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,27 @@
 import { useState } from 'react';
 
-function Square({value}) {
+function Square({ value }) {
   return <button className="square">{value}</button>;
 }
-//   const [value, setValue] = useState(null);
 
-//   function handleClick() {
-//     setValue('X');
-//   }
-//   return (
-//     <button className="square"
-//     onClick={handleClick}
-//     >
-//       {value}
-//     </button>
-//   );
-// }
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-      <Square value={squres[0]}/>
-      <Square value={squres[1]}/>
-      <Square value={squres[2]}/>
+      <Square value={squares[0]}/>
+      <Square value={squares[1]}/>
+      <Square value={squares[2]}/>
       </div>
       <div className="board-row">
-      <Square value={squres[3]}/>
-      <Square value={squres[4]}/>
-      <Square value={squres[5]}/>
+      <Square value={squares[3]}/>
+      <Square value={squares[4]}/>
+      <Square value={squares[5]}/>
       </div>
       <div className="board-row">
-      <Square value={squres[6]}/>
-      <Square value={squres[7]}/>
-      <Square value={squres[8]}/>
+      <Square value={squares[6]}/>
+      <Square value={squares[7]}/>
+      <Square value={squares[8]}/>
       </div>
     </>
   );
