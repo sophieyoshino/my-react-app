30076a49c1350b2e3b66a2e7f5d845cb10a9464a
Sophie Yoshino <s.s.yoshino@wustl.edu>
2025-11-10 13:40:19 -0600

    handlePlay funct added

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,11 @@ export default function Game() {
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
+
+  function handlePlay(nextSquares) {
+    //TODO
+  }
+  
   return (
     <div className="game">
       <div className="game-board">
