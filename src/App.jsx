74e89fb7fef39a12d714398f2fd54a20f4e1c430
Sophie Yoshino <s.s.yoshino@wustl.edu>
2025-11-10 13:15:55 -0600

    values readded but with the array

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,39 @@
 import { useState } from 'react';
 
-function Square() {
-  const [value, setValue] = useState(null);
-
-  function handleClick() {
-    setValue('X');
-  }
-  return (
-    <button className="square"
-    onClick={handleClick}
-    >
-      {value}
-    </button>
-  );
+function Square({value}) {
+  return <button className="square">{value}</button>;
 }
+//   const [value, setValue] = useState(null);
+
+//   function handleClick() {
+//     setValue('X');
+//   }
+//   return (
+//     <button className="square"
+//     onClick={handleClick}
+//     >
+//       {value}
+//     </button>
+//   );
+// }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squres[0]}/>
+      <Square value={squres[1]}/>
+      <Square value={squres[2]}/>
       </div>
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squres[3]}/>
+      <Square value={squres[4]}/>
+      <Square value={squres[5]}/>
       </div>
       <div className="board-row">
-      <Square />
-      <Square />
-      <Square />
+      <Square value={squres[6]}/>
+      <Square value={squres[7]}/>
+      <Square value={squres[8]}/>
       </div>
     </>
   );
