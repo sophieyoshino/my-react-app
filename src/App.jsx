1f0080017e0e9cc941368685d86ad9c16dcc74f4
Sophie Yoshino <s.s.yoshino@wustl.edu>
2025-11-10 13:55:09 -0600

    move key added

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ export default function Game() {
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     );
