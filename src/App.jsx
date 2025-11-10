e84c856a95ebb7cae03f0aaaefafa612f50982b2
Sophie Yoshino <s.s.yoshino@wustl.edu>
2025-11-10 13:10:47 -0600

    X appears when clicked on square

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function Square() {
   const [value, setValue] = useState(null);
 
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
   return (
     <button className="square"
